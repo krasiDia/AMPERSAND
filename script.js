4ed1ca1e71543f8ab2e82fd4f6c5ae508cb9571b
krasiDia <128245934+krasiDia@users.noreply.github.com>
2023-03-19 18:26:21 +0400

    Add files via upload
    --- second commit ---
    адаптация

--- script.js
+++ script.js
@@ -22,3 +22,85 @@ navigator.geolocation.getCurrentPosition(function(position){
         
     });
 });
+
+let menu = document.querySelector(".menu_small_icon"); 
+let pos = 0; 
+menu.addEventListener("click", function () { 
+  if (pos === 0) { 
+    open(); 
+  } else { 
+    close(); 
+  } 
+}); 
+ 
+let start = 0; 
+let end = 0; 
+let screenWidth = window.screen.width
+ 
+$(".container").on("touchstart", function (e) { 
+  start = e.originalEvent.touches[0].pageX; 
+}); 
+$(".container").on("touchend", function (e) { 
+  end = e.originalEvent.changedTouches[0].pageX; 
+  
+  if (end - start >= 100 && pos === 0 && start <= 20) { 
+    open(); 
+  } else if (start - end >= 100 && pos === 1 && start >= screenWidth - 20) { 
+    close(); 
+  } 
+}); 
+function open() { 
+    anime({ 
+        targets: ".menu-small", 
+        translateX: ["100%", "0"], 
+        easing: "easeInOutQuint", 
+        direction: "alternate", 
+        duration: 1000, 
+        loop: false, 
+    }); 
+    pos = 1; 
+    anime({
+        targets: '.menu_small_icon',
+        rotate: 90,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    })
+    anime({
+        targets: '.stickMENU',
+        rotate: 180,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    })
+} 
+function close() { 
+    anime({ 
+        targets: ".menu-small", 
+        translateX: ["0", "100%"], 
+        easing: "easeInOutQuint", 
+        direction: "alternate", 
+        duration: 1000, 
+        loop: false, 
+    }); 
+    pos = 0; 
+    anime({
+        targets: '.menu_small_icon',
+        rotate: -180,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    })
+    anime({
+        targets: '.stickMENU',
+        rotate: -180,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        duration: 1000,
+        loop: false
+    })
+}
+ 
